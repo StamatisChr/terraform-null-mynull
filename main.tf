644cfa072cf90ae6f27b111d3b8c3aaae84994b0
StamatisChr <166624731+StamatisChr@users.noreply.github.com>
2025-12-11 15:55:07 +0100

    Implement resource dependencies with null_resource and time_sleep
    Add null_resource and time_sleep resources for sequencing.

--- main.tf
+++ main.tf
@@ -86,3 +86,16 @@ resource "random_pet" "pet3" {
 output "pet3" {
   value = random_pet.pet3.*.id
 }
+
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "30s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
