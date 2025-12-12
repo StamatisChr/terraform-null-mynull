42d449cf4cfcc21f0f9bc499f790925fcda66125
StamatisChr <166624731+StamatisChr@users.noreply.github.com>
2025-12-12 12:05:52 +0100

    Clean up unused resources in main.tf
    Removed unused null_resource and time_sleep resources.

--- main.tf
+++ main.tf
@@ -65,19 +65,6 @@ resource "random_pet" "pet3" {
 }
 
 
-resource "null_resource" "previous" {}
-
-resource "time_sleep" "wait_30_seconds" {
-  depends_on = [null_resource.previous]
-
-  create_duration = "30s"
-}
-
-# This resource will create (at least) 30 seconds after null_resource.previous
-resource "null_resource" "next" {
-  depends_on = [time_sleep.wait_30_seconds]
-}
-
 
 output "pet3" {
   value = random_pet.pet3.*.id
